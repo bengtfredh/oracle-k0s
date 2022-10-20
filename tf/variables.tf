29ff9461ac84c237c9b4f86aa0f230698261dedd
Bengt <bengt@fredhs.net>
2022-10-21 01:07:28 +0200

    Add metallb

---
+++ tf/variables.tf
@@ -0,0 +1,12 @@
+variable "client_certificate" {
+  type = string
+}
+variable "client_key" {
+  type = string
+}
+variable "cluster_ca_certificate" {
+  type = string
+}
+variable "kubernetes_host" {
+  type = string
+}
