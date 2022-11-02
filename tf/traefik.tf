2e6b14a860b40bde23e0aa14cf12ae022ce8d91a
Bengt <bengt@fredhs.net>
2022-11-02 23:56:22 +0100

    traefik hub

--- tf/traefik.tf
+++ tf/traefik.tf
@@ -19,27 +19,6 @@ resource "helm_release" "traefik" {
                  providers:
                    kubernetesIngress:
                      enabled: false
-                 globalArguments:
-                   - "--global.checknewversion=false"
-                   - "--global.sendanonymoususage=false"
-                 tlsOptions:
-                   default:
-                     minVersion: VersionTLS12
-                     cipherSuites:
-                       - TLS_AES_256_GCM_SHA384
-                       - TLS_CHACHA20_POLY1305_SHA256
-                       - TLS_AES_128_GCM_SHA256
-                       - TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256
-                       - TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256
-                       - TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256
-                       - TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256
-                       - TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384
-                       - TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384
-                       - TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256
-                 tlsStore:
-                   default:
-                     defaultCertificate:
-                       secretName: fredhs-net-cert
                  service:
                    type: ClusterIP
                  affinity:
@@ -57,57 +36,6 @@ resource "helm_release" "traefik" {
             EOT
   ]
 
-}
-
-resource "kubectl_manifest" "traefik_dashboard_auth" {
-  depends_on = [helm_release.traefik]
-  yaml_body  = <<YAML
-apiVersion: v1
-kind: Secret
-metadata:
-  name: traefik-dashboard-auth
-  namespace: traefik
-type: Opaque
-data:
-  users: ${var.traefik_dashboard_auth}
-YAML
-}
-
-resource "kubectl_manifest" "traefik_dashboard_middleware" {
-  depends_on = [helm_release.traefik]
-  yaml_body  = <<YAML
-apiVersion: traefik.containo.us/v1alpha1
-kind: Middleware
-metadata:
-  name: traefik-dashboard-basicauth
-  namespace: traefik
-spec:
-  basicAuth:
-    secret: traefik-dashboard-auth
-YAML
-}
-
-resource "kubectl_manifest" "traefik_dashboard_ingressroute" {
-  depends_on = [helm_release.traefik]
-  yaml_body  = <<YAML
-apiVersion: traefik.containo.us/v1alpha1
-kind: IngressRoute
-metadata:
-  name: dashboard
-  namespace: traefik
-spec:
-  entryPoints:
-  - websecure
-  routes:
-  - kind: Rule
-    match: Host(`oracle-k0s-traefik.fredhs.net`) && (PathPrefix(`/dashboard`) || PathPrefix(`/api`))
-    middlewares:
-    - name: traefik-dashboard-basicauth
-      namespace: traefik
-    services:
-    - kind: TraefikService
-      name: api@internal
-YAML
 }
 
 resource "helm_release" "traefik_hub" {
