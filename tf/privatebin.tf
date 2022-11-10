c71ed0c1a7f87d63a6e211a9963d7438a5207ed0
Bengt <bengt@fredhs.net>
2022-11-10 18:52:39 +0100

    Add privatebin

---
+++ tf/privatebin.tf
@@ -0,0 +1,28 @@
+resource "helm_release" "privatebin" {
+  name      = "privatebin"
+  namespace = "privatebin"
+
+  repository       = "https://privatebin.github.io/helm-chart"
+  chart            = "privatebin"
+  create_namespace = true
+
+  values = [<<-EOT
+                 configs:
+                   conf.php: |-
+                     [model]
+                     class = Filesystem
+                     [model_options]
+                     dir = PATH "data"
+                     [main]
+                     fileupload = true
+                     template = "bootstrap-dark"
+                     syntaxhighlightingtheme = "sons-of-obsidian"
+            EOT
+  ]
+
+  set {
+    name  = "controller.kind"
+    value = "StatefulSet"
+  }
+
+}
